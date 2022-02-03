ad1c61837c9232752852ccf8d408cdf380dbc847
dextrodeath <ramseyjohnjoseph@gmail.com>
2022-02-02 23:10:16 -0600

    about page image + text added

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -16,16 +16,34 @@ const About = () => {
         >
           {/* page content */}
           <div className='flex justify-center items-center w-full h-full'>
-            <div className='flex' style={{
-              width: '80%',
-              height: '80%',
-              borderTop: '1px solid #333',
-              borderBottom: '1px solid #333',
-              paddingTop: '25px',
-              paddingBottom: '25px',
-            }}>
+            <div style={{
+                width: '60%',
+                height: '60%',
+              }}>
+              <div className='flex' style={{
+                borderTop: '1px solid #333',
+                borderBottom: '1px solid #333',
+                paddingTop: '25px',
+                paddingBottom: '25px',
+              }}>
                 <img className='w-1/2' src={aboutImg} />
-              <div className='w-1/2 p-8'>lorum ipsum lorum ipsum lorum ipsum lorum ipsum lorum ipsum lorum ipsum lorum ipsum lorum ipsum</div>
+                <div className='w-1/2 p-8 flex justify-between flex-col'>
+                  <p className='italic'>About</p>
+                  <p>Hello, I'm Michelle.</p>
+                  <p>
+                    I'm a <span className='italic'>creative storyteller</span> that aims <br />
+                    to <span className='italic'>empathize</span> and <span className='italic font-bold'>problem solve <br />
+                      visually.</span>
+                  </p>
+                  <p>
+                    With over three years of experience <br />
+                    that spreads across brand identity, <br />
+                    visual communication, and <br />
+                    UI/UX.
+                  </p>
+                </div>
+              </div>
+              {/* button here */}
             </div>
           </div>
 
