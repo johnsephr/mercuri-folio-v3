9717f89a5784db3612f33d4e1d9ccd1e1ea3fb00
dextrodeath <ramseyjohnjoseph@gmail.com>
2022-03-06 20:38:27 -0600

    replace dashes with "em dash" thing

--- src/components/LandingBanners/ToTBanner.js
+++ src/components/LandingBanners/ToTBanner.js
@@ -5,7 +5,13 @@ const ToTBanner = () => {
     <div className="h-full w-full grid pt-12">
       <div className="row-span-1 w-full" />
       <h1 className="text-5xl px-24 text-left row-span-2" style={{ color: 'white' }}>
-        <p className="text-xl text-left italic pb-5" style={{color: 'white'}}>Creative Direction --- Branding Identity --- Design System</p>
+        <p className="text-xl text-left italic pb-5" style={{ color: 'white' }}>
+          Creative Direction
+          <hr className="w-10 h-1 bg-black inline-block mx-1" />
+          Branding Identity
+          <hr className="w-10 h-1 bg-black inline-block mx-1" />
+          Design System
+        </p>
 
         <span className="font-bold">Tunnel of Trees</span> is a music blog dedicated<br />
         to taking music and turning it into a serious<br />
