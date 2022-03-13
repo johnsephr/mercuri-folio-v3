bfc48f6d57d4ce6a1b8876d4589298d5edd388cb
dextrodeath <ramseyjohnjoseph@gmail.com>
2022-03-13 15:32:49 -0500

    hr error - tot banner

--- src/components/LandingBanners/ToTBanner.js
+++ src/components/LandingBanners/ToTBanner.js
@@ -5,13 +5,13 @@ const ToTBanner = () => {
     <div className="h-full w-full grid pt-12">
       <div className="row-span-1 w-full" />
       <h1 className="text-5xl px-24 text-left row-span-2" style={{ color: 'white' }}>
-        <p className="text-xl text-left italic pb-5" style={{ color: 'white' }}>
-          Creative Direction
+        <div className="text-xl text-left italic pb-5" style={{ color: 'white' }}>
+          <span>Creative Direction</span>
           <hr className="w-10 h-1 bg-black inline-block mx-1" />
-          Branding Identity
+          <span>Branding Identity</span>
           <hr className="w-10 h-1 bg-black inline-block mx-1" />
-          Design System
-        </p>
+          <span>Design System</span>
+        </div>
 
         <span className="font-bold">Tunnel of Trees</span> is a music blog dedicated<br />
         to taking music and turning it into a serious<br />
