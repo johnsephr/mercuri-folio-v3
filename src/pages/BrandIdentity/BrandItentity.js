aba209541baf156504cae41e90fb98a0e3c6d183
dextrodeath <ramseyjohnjoseph@gmail.com>
2022-03-19 16:15:51 -0500

    logo display img added to files, style fix img/bio

--- src/pages/BrandIdentity/BrandItentity.js
+++ src/pages/BrandIdentity/BrandItentity.js
@@ -12,6 +12,7 @@ import brandIdentityRolesListData from '../../data/roles/brandIdentityRolesListD
 import dueDate from '../../assets/images/brand-identity/due-date.png';
 import scribbledPortrait from '../../assets/images/brand-identity/scribbled-portrait.png';
 import darkStar from '../../assets/icons/star/darkstar.svg';
+import logoDisplay from '../../assets/images/brand-identity/logo-display.png';
 
 const BrandIdentity = () => {
   return (
@@ -28,14 +29,14 @@ const BrandIdentity = () => {
           <div className="w-3/5">
             <p className="italic mt-6 inline-block w-1/2">title of project:</p><p className="italic bold mt-6 inline-block w-1/2">due date:</p><br />
             <h1 className="text-5xl inline-block w-1/2">personal <span className="italic">identity</span></h1>
-            <div className="w-1/2 inline-block"><img className="w-1/3 inline-block" src={dueDate} style={{ transform: 'translateY(-10px)' }} /></div>
+            <div className="w-1/2 inline-block"><img className="w-1/3 inline-block" src={dueDate} style={{ transform: 'translateY(-10px) translateX(-32px)', }} /></div>
           </div>
         </div>
 
         {/* image and bio */}
-        <div>
+        <div className="flex flex-row">
           <div className="w-1/5 inline-block"><img src={scribbledPortrait} className="w-full" /></div>
-          <div className="w-1/5 ml-10 inline-block h-full flex flex-col">
+          <div className="w-1/5 ml-10 inline-block h-full flex-col">
             {/* star */}
             <div className="h-8 mb-4"><img src={darkStar} className="h-full" /></div>
             {/* bio */}
@@ -46,6 +47,9 @@ const BrandIdentity = () => {
             </p>
           </div>
         </div>
+
+        {/* logo display */}
+
 
       </div>
     </div>
